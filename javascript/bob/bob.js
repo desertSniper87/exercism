b9f62a25001c78f05545ff7abfbf222abae389f3
desertsniper87 <torshobuet@gmail.com>
2020-01-15 12:28:23 +0600

    js new

---
+++ javascript/bob/bob.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const hey = (message) => {
+  throw new Error("Remove this statement and implement this function");
+};
