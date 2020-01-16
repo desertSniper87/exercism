121db24c63d33978629491411153225bf2cf3868
desertsniper87 <torshobuet@gmail.com>
2020-01-16 16:27:42 +0600

    bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,8 +1,11 @@
 export const hey = (message) => {
-  if (message.slice(-1) === '?')
-    return 'Sure.';
-  if (message.toUpperCase() === message)
-    return 'Whoa, chill out!';
-  else 
-    return 'Whatever.';
+  if (message.match(/[a-zA-Z0-9?]+/)){
+    if (message.trim().slice(-1) === '?')
+      return (message.includes("HELL") ? "Calm down, I know what I\'m doing!" : 'Sure.');
+    else if (message.match(/[A-Z]/) && message.toUpperCase() === message)
+      return 'Whoa, chill out!';
+    else 
+      return 'Whatever.';
+  } else 
+    return 'Fine. Be that way!';
 };
