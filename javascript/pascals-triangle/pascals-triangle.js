ff13368949388b7f80fddc3a833a132a57060a9d
desertsniper87 <torshobuet@gmail.com>
2020-01-30 13:11:52 +0600

    Pascals triangle soln

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const rows = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const rows = numRows => {
+  let pascalTriangle = [];
+  
+  for (let i = 0; i < numRows; i++) { 
+    pascalTriangle[i] = new Array(i+1);
+    
+    for (var j = 0; j < i+1; j++) {            
+      if (j === 0 || j === i) {
+        pascalTriangle[i][j] = 1;
+      } else {
+        pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+      }
+    }
+  }
+  
+  return pascalTriangle;
+}
