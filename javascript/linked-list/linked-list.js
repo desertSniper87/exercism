a025f9c9f35a201a59816e73d2f4d1b78ba361ab
desertsniper87 <torshobuet@gmail.com>
2020-04-25 10:33:38 +0600

    linked list complete

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Linked List' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class LinkedList {
   constructor() {
     this.head = null;
@@ -11,11 +6,11 @@ export class LinkedList {
   push(el) {
     let n = new Node(el, null);
 
-    if (this.head == null)
+    if (this.head === null)
       this.head = n;
     else {
-      let cur = this.data;
-      while (cur.next != null){
+      let cur = this.head;
+      while (cur.next != null) {
         cur = cur.next;
       }
       cur.next = n;
@@ -23,23 +18,66 @@ export class LinkedList {
   }
 
   pop() {
-    return this.data.val;
+    let cur = this.head;
+    let prev = null;
+    while (cur.next != null) {
+      prev = cur;
+      cur = cur.next;
+    }
+    if (prev != null)
+      prev.next = null;
+    else
+      this.head = null;
+
+    return cur.val;
   }
 
   shift() {
-    throw new Error("Remove this statement and implement this function");
+    let cur = this.head;
+    this.head = cur.next;
+    return cur.val;
   }
 
-  unshift() {
-    throw new Error("Remove this statement and implement this function");
+  unshift(el) {
+    let n = new Node(el, this.head);
+    this.head = n;
   }
 
-  delete() {
-    throw new Error("Remove this statement and implement this function");
+  delete(el) {
+    let cur = this.head;
+    let prev = null;
+    if (cur.val === el)
+      if (this.head.next === null)
+        this.head = null;
+      else 
+        this.head = this.head.next;
+
+    while (cur.next != null) {
+      prev = cur;
+      cur = cur.next;
+
+      if (cur.val === el) {
+        prev.next = cur.next;
+        break
+      }
+    }
   }
 
   count() {
-    throw new Error("Remove this statement and implement this function");
+    let cur = this.head; 
+    let cnt = 1;
+
+    if (cur === null)
+      return 0;
+    else if (cur.next === null)
+      return 1;
+
+    while (cur.next!=null) {
+      cur = cur.next;
+      cnt++;
+    }
+    return cnt;
+
   }
 }
 
